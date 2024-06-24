15d59350ca3fa8bbd0eac72e6df04acdf22af055
vonmartiner <martiner.thomas@yahoo.de>
2024-06-24 16:24:16 +0200

    nochmal richtig gestellt

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
+import 'package:stream_gold_rate/src/app.dart';
 
-class GoldScreen extends StatelessWidget {
-  const GoldScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 3, 3, 2),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildHeader(context),
-              const SizedBox(height: 20),
-              Text('Live Kurs:',
-                  style: Theme.of(context).textTheme.headlineMedium),
-              const SizedBox(height: 20),
-              StreamBuilder<double>(
-                stream: getGoldPriceStream(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    return const Text(
-                      'Fehler beim Laden des Goldpreises',
-                      style: TextStyle(color: Colors.red),
-                    );
-                  } else if (snapshot.hasData) {
-                    return Text(
-                      NumberFormat.simpleCurrency(locale: 'de_DE')
-                          .format(snapshot.data),
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineLarge!
-                          .copyWith(
-                              color: Theme.of(context).colorScheme.primary),
-                    );
-                  } else {
-                    return const Text('Keine Daten verfügbar');
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Row _buildHeader(BuildContext context) {
-    return Row(
-      children: [
-        const Image(
-          image: AssetImage('assets/bars.png'),
-          width: 100,
-        ),
-        Text('Gold', style: Theme.of(context).textTheme.headlineLarge),
-      ],
-    );
-  }
+void main() {
+  runApp(const App());
 }
--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
 
 class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    /// Platzhalter für den Goldpreis
-    /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    const double goldPrice = 69.22;
-
     return SafeArea(
       child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 3, 3, 2),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
@@ -21,14 +19,30 @@ class GoldScreen extends StatelessWidget {
               Text('Live Kurs:',
                   style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
+              StreamBuilder<double>(
+                stream: getGoldPriceStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return const Text(
+                      'Fehler beim Laden des Goldpreises',
+                      style: TextStyle(color: Colors.red),
+                    );
+                  } else if (snapshot.hasData) {
+                    return Text(
+                      NumberFormat.simpleCurrency(locale: 'de_DE')
+                          .format(snapshot.data),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineLarge!
+                          .copyWith(
+                              color: Theme.of(context).colorScheme.primary),
+                    );
+                  } else {
+                    return const Text('Keine Daten verfügbar');
+                  }
+                },
               ),
             ],
           ),
